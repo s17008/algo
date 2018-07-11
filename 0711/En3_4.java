ed78ec3beff636bb63e2c5a513855bbc38b5b295
s17008 <s17008@std.it-college.ac.jp>
2018-07-11 15:58:15 +0900

    add En3_4

---
+++ 0711/En3_4.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+class En3_4 {
+	static int binSearch(int[] a, int n, int key) {
+		System.out.print("   |");
+		for (int i = 0; i < n; i++)
+			System.out.printf("%4d", i);
+		System.out.println();
+		System.out.print("---+");
+
+		for (int i = 0; i < 4 * n + 2; i++)
+			System.out.print("-");
+		System.out.println();
+		int pl = 0;
+		int pr = n - 1;
+
+		do {
+			int pc = (pl+pr)/2;
+			System.out.print("   |");
+			if (pl != pc)
+				System.out.printf(String.format("%%%ds<-%%%ds+", (pl * 4) + 1, (pc-pl) * 4), "", "");
+			else
+				System.out.printf(String.format("%%%ds<-+", pc * 4 + 1), "");
+			if (pc != pr)
+				System.out.printf(String.format("%%%ds->\n", (pr - pc) * 4 - 2), "");
+			else
+				System.out.println("->");
+			System.out.printf("%3d|", pc);
+			for (int i = 0; i < n; i++)
+				System.out.printf("%4d", a[i]);
+			System.out.println("\n   |");
+			if (a[pc] == key)
+				return pc;
+			else if (a[pc] < key)
+				pl = pc + 1;
+			else
+				pr = pc - 1;
+		} while (pl <= pr);
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("要素数:");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];
+		System.out.println("照準に入力してください。");
+		System.out.print("x[0]:");
+		x[0] = stdIn.nextInt();
+
+		for (int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "]:");
+				x[i] = stdIn.nextInt();
+			} while (x[i] < x[i - 1]);
+		}
+		System.out.print("探す値:");
+		int ky = stdIn.nextInt();
+		int idx = binSearch(x, num, ky);
+
+		if (idx == -1)
+			System.out.println("その要素はありません。");
+		else
+			System.out.println(ky + "はx[" + idx + "] にあります。");
+	}
+}
