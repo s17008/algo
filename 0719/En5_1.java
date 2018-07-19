8cc4d06ffa80ee687ca8234023a06bc5bce7690f
s17008 <s17008@std.it-college.ac.jp>
2018-07-19 16:20:38 +0900

    add En5_1

---
+++ 0719/En5_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class En5_2 {
+	static int factorial(int n) {
+		int total = 1;
+		for (int i = 1; i <= n; i++)
+			total *= i;
+		return total;
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("整数を入力せよ:");
+		int x = stdIn.nextInt();
+		System.out.println(x + "の階乗は" + factorial(x) + "です。");
+	}
+}
